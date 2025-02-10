9583b7b08e6b4fce0001a84a2325011fce668b57
pete <work@pete-hamlin.com>
2025-02-10 21:01:00 +0000

    chore: WIP files & gitignore

--- src/plugin.cpp
+++ src/plugin.cpp
@@ -6,6 +6,16 @@
 #include <albert/util.h>
 #include <ServerManager>
 #include <Session>
+#include <Akonadi/Collection>
+#include <Akonadi/CollectionFetchJob>
+#include <Akonadi/CollectionFetchScope>
+#include <Akonadi/ItemFetchJob>
+#include <Akonadi/ItemFetchScope>
+#include <Akonadi/Item>
+#include <KContacts/Addressee>
+#include <QTextStream>
+// #include <iostream>
+
 ALBERT_LOGGING_CATEGORY("contacts")
 using namespace albert;
 using namespace Akonadi;
@@ -14,8 +24,15 @@ using namespace std;
 
 vector<RankItem> Plugin::handleGlobalQuery(const Query *query)
 {
-
   vector<RankItem> results;
+    QObject::connect(fetchJob, &Akonadi::CollectionFetchJob::result, [](KJob *job) {
+        if (job->error()) {
+            qCritical() << "Error fetching collections:" << job->errorString();
+            QCoreApplication::quit();
+            return;
+        }
+
+
 
   return results;
 }
